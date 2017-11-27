6d6d95e1d44f293458edecae027bc09cc4910d9b
haner27 <369685930@qq.com>
2017-11-27 17:37:29 +0800

    html basic clean

--- utils/string_utils.py
+++ utils/string_utils.py
@@ -31,3 +31,22 @@ def get_unique_name(ext):
     now = datetime.now()
     return '{0}{1}.{2}'.format(now.strftime('%Y%m%d%H%M%S'), randint(100, 999), ext)
 
+
+def html_basic_clean(html):
+    if not html:
+        return html
+    replaces = [
+        # style=\"font-size: 16px;\", style=\"font-size:14px;font-family:宋体;color:#333333\"
+        (r'style="(?:;?font[^:;"]*:[^";]*;?)*"', ''),
+        (r'font-(?:size|family):[^;"]*;?', ''),
+        (r'&nbsp;', ' '),
+        (r'\u200b', ''),
+        (r'</?o:p>', ''),  # <o:p></o:p>
+        (r'(<span[^>]*)lang="[^"]*"', r'\1'),  # <span lang=\"EN-US\">
+        (r'(<table[^>]*)align="[l|r][^"]*"', r'\1'),  # <table align="left|right"> 过滤 align="left"或者align="right"
+        (r'data-bind[\s]*=[\s]*"[^"]+"', ''),  # 过滤data-bind标签
+        (r'<(/)?st1:[^>]*>', ''),  # 过滤<st1:country-region>标签
+    ]
+    for reg, rep in replaces:
+        html = re.sub(reg, rep, html)
+    return html.strip()
