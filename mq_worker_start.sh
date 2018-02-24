563f31d0f60e968ec00e667a73dddfd279252ff3
haner27 <369685930@qq.com>
2018-02-24 11:45:37 +0800

    启动mq消费者脚本 添加autoreload选项

--- mq_worker_start.sh
+++ mq_worker_start.sh
@@ -1,4 +1,4 @@
-# 启动, ps：-c是并发数, -c1是阻塞队列
-nohup python mq\celery_worker.py worker -c2 -n common_worker -Q common -l info > /data/log/celery_common_worker.log &
-nohup python mq\celery_worker.py worker -c2 -n email_worker -Q email -l info > /data/log/celery_email_worker.log &
-nohup python mq\celery_worker.py worker -c2 -n file_worker -Q file -l info > /data/log/celery_file_worker.log &
+# 启动, ps：1.-c是并发数, -c1是阻塞队列;2.--autoreload 自动加载修改, 该选项在4.0版本后被删除
+nohup python mq\celery_worker.py worker --autoreload -c2 -n common_worker -Q common -l info > /data/log/celery_common_worker.log &
+nohup python mq\celery_worker.py worker --autoreload -c2 -n email_worker -Q email -l info > /data/log/celery_email_worker.log &
+nohup python mq\celery_worker.py worker --autoreload -c2 -n file_worker -Q file -l info > /data/log/celery_file_worker.log &
